28292767275379d063240b0520028e0c77b907f8
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-08 13:30:45 +0700

    Path Variable dan Query String

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 
 	router.GET("/", rootHandler)
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/books", queryHandler)
 
 	router.Run(":8080")
 
@@ -28,5 +30,25 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":    "Hello World",
 		"subtitle": "Belajar Golang",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.DefaultQuery("title", "antek async")
+	price := c.Query("price")
+
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
 	})
 }
