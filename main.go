fea90da7721f65d0fd72ac4d6f03a1b07e274126
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-08 11:52:13 +0700

    Handler

--- main.go
+++ main.go
@@ -10,20 +10,23 @@ func main() {
 
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "andhika juliawan",
-			"bio":  "software engineer",
-		})
-	})
-
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":    "Hello World",
-			"subtitle": "Belajar Golang",
-		})
-	})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
 
 	router.Run(":8080")
 
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "andhika juliawan",
+		"bio":  "software engineer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Hello World",
+		"subtitle": "Belajar Golang",
+	})
 }
