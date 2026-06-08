2c9bdf26268f76f7cb7e0e531104436c399be370
andhika juliawan <andhikajuliawan@andhikas-MacBook-Air.local>
2026-06-08 11:42:31 +0700

    Berkenalan dengan Gin

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"name": "andhika juliawan",
+			"bio":  "software engineer",
+		})
+	})
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"title":    "Hello World",
+			"subtitle": "Belajar Golang",
+		})
+	})
+
+	router.Run(":8080")
+
+}
